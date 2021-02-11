5d951e2d2896ffba628a69444f58139152041290
AdityaKakkad <74364910+AdityaKakkad@users.noreply.github.com>
2021-02-11 09:30:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var canvas = new fabric.Canvas("myCanvas");
+block_height = 10;
+block_width = 10;
+var player_object = "";
+var block_image_object = "";
+player_y = 10;
+player_x = 10;
+
+function player_update(){
+    fabric.Image.fromURL("groot.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+        top:player_y,
+        left:player_x
+        });
+        canvas.add(player_object);
+    });
+}
+
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_image_object);
+    });
+}
